18db17289e2170106da0493a0d34d95a718ac776
GoranTopic <telegonicaxx@live.com>
2022-06-21 21:50:35 -0700

    working windows

--- src/world/world.js
+++ src/world/world.js
@@ -13,7 +13,6 @@ import { loadMarker } from '../components/marker';
 //import { createMeshGroup } from '../components/meshGroup';
 //import { loadDancerDude } from '../components/dancerDude'; 
 //import { loadBirds } from '../components/birds/birds';
-
 import { createRenderer } from '../systems/renderer';
 import { Loop } from '../systems/Loop';
 // import controller
@@ -23,90 +22,89 @@ import { Resizer } from '../systems/Resizer';
 
 let camera, renderer, scene, loop, controller, plane, lights, grid, player, marker;
 
-
 class World {
-		constructor(container){
-				// create camera
-				camera = createCamera();
-				// create scene
-				scene = createScene();
-				// create renderer
-				renderer = createRenderer();
-				// create loop 
-				loop = new Loop(camera, scene, renderer);
-				// render to the DOM
-				container.appendChild(renderer.domElement);
-				//create ligths 
-				lights = createLights();
-				// create ground
-				plane = createPlane();
-				// create grid
-				grid = createGrid();
+	constructor(container) {
+		// create camera
+		camera = createCamera();
+		// create scene
+		scene = createScene();
+		// create renderer
+		renderer = createRenderer();
+		// create loop 
+		loop = new Loop(camera, scene, renderer);
+		// render to the DOM
+		container.appendChild(renderer.domElement);
+		//create ligths 
+		lights = createLights();
+		// create ground
+		plane = createPlane();
+		// create grid
+		grid = createGrid();
 
-				// add cube to scene 
-				scene.add( 
-						...lights,
-						plane, 
-						grid,
-				);
-				// add controller			
-				controller = addController(camera, renderer);
+		// add cube to scene 
+		scene.add(
+			...lights,
+			plane,
+			grid,
+		);
+		// add controller			
+		controller = addController(camera, renderer);
 
-				// add to animation loop
-				// add controller
-				loop.add( 
-						controller, 
-						plane,
-				)
+		// add to animation loop
+		// add controller
+		loop.add(
+			controller,
+			plane,
+		)
 
-				// load the mouse marker 
-				marker = loadMarker();
-				scene.add(marker);
-				loop.add(marker);
+		// load the mouse marker 
+		marker = loadMarker();
+		scene.add(marker);
+		loop.add(marker);
 
-				// add stats
-				const stats = createStats();
-				container.appendChild( stats.dom );
-				loop.add(stats);
+		// add stats
+		const stats = createStats();
+		container.appendChild(stats.dom);
+		loop.add(stats);
 
-		}
+	}
 
-		async init() { // load async objects
+	async init() { // load async objects
 
-				player = await loadPlayer();
-				scene.add(player);
-				loop.add(player);
+		player = await loadPlayer();
+		scene.add(player);
+		loop.add(player);
 
-				//terrain = await loadTerrain();
-				//console.log('terrain', terrain);
-				//scene.add(terrain);
-				
+		//terrain = await loadTerrain();
+		//console.log('terrain', terrain);
+		//scene.add(terrain);
 
-		}
-		
-		// start the loop
-		start = () => loop.start()
-		
-		// stop the loop
-		stop = () => loop.stop()
 
-		// render the world
-		render = () =>
-				renderer.render(scene, camera);
+	}
+
+	// start the loop
+	start = () => loop.start()
+
+	// stop the loop
+	stop = () => loop.stop()
+
+	// render the world
+	render = () =>
+		renderer.render(scene, camera);
 
 }
 
 
-export { 
-		World, 
-		camera, 
-		renderer, 
-		scene, 
-		loop, 
-		controller, 
-		plane, 
-		lights, 
-		grid, 
-		player,
-		marker,
+export {
+	World,
+	camera,
+	renderer,
+	scene,
+	loop,
+	controller,
+	plane,
+	lights,
+	grid,
+	player,
+	marker,
 };
